e5df1921998b07773db3ca8c30901d82ea3a561e
Bensaad <ben@MacBook-Pro-de-Bensaad.local>
2024-11-07 10:21:08 +0100

    update vite.conf

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    target: ["es2020", "chrome87", "edge88", "firefox78", "safari14"],
-  },
 });
